563ce4acb7ac63d236747e25adad651f4d17c871
Katherine <katherine.szelag@mail.utoronto.ca>
2019-11-27 18:02:29 +0000

    letter object

---
+++ countLetters.js
@@ -0,0 +1,43 @@
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
+
+const countLetters = function(string) {
+  let letterObject = {};
+  let noSpaces = string.toLowerCase().split(" ").join("");
+
+  for (let letter of noSpaces) {
+    if (letterObject.hasOwnProperty(letter) === false) {
+      letterObject[letter] = 1;
+    } else {
+      letterObject[letter] = letterObject[letter] + 1
+    }
+  }
+  return letterObject; 
+}
+console.log(countLetters("lighthouse in the house"));
+
+// console.log(assertEqual(countLetters("lighthouse in the house"), {
+//   l: 1,
+//   i: 2,
+//   g: 1,
+//   h: 4,
+//   t: 2,
+//   o: 2,
+//   u: 2,
+//   s: 2,
+//   e: 3,
+//   n: 1,
+// }));
+
+// let obj = {
+//   car: 2
+// };
+// obj.car += 1;
+
+// console.log(obj);
+// console.log(typeof(obj.car));
